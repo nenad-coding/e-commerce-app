73928346c6b30a953a9e9f12820bfc141b09b3d3
Nenad Rakovic Coding <nenadrakovic.coding@gmail.com>
2025-03-01 14:51:17 +0100

    Small adjustments

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
 } from '@/components/ui/card'
 import { CirclePlus } from 'lucide-react'
+import Link from 'next/link'
 
 export default async function UsersPage() {
   const usersData = await fetch('https://fakestoreapi.com/users').then(
@@ -28,17 +29,38 @@ export default async function UsersPage() {
           key={index}
           className="min-h-[270px] flex flex-col justify-between shadow-lg">
           <CardHeader className="flex">
-            <CardDescription className="mx-auto">
+            <CardDescription className="mx-auto font-bold text-black text-lg">
               <span className="capitalize">{user.name.firstname}</span>{' '}
               <span className="capitalize">{user.name.lastname}</span>
             </CardDescription>
           </CardHeader>
           <CardContent className="grow">
             <div>
-              <p>City: {user.address.city}</p>
-              <p>Street: {user.address.street}</p>
-              <p>Street number: {user.address.number}</p>
-              <p>Zipcode: {user.address.zipcode}</p>
+              <p className="text-gray-500">{user.email}</p>
+              <p>
+                City:{' '}
+                <span className="font-semibold capitalize">
+                  {user.address.city}
+                </span>
+              </p>
+              <p>
+                Street:{' '}
+                <span className="font-semibold capitalize">
+                  {user.address.street}
+                </span>
+              </p>
+              <p>
+                Street number:{' '}
+                <span className="font-semibold capitalize">
+                  {user.address.number}
+                </span>
+              </p>
+              <p>
+                Zipcode:{' '}
+                <span className="font-semibold capitalize">
+                  {user.address.zipcode}
+                </span>
+              </p>
             </div>
           </CardContent>
           <CardFooter className="flex gap-x-4 justify-center">
@@ -47,7 +69,9 @@ export default async function UsersPage() {
               className="h-[35px] shadow-md text-red-500">
               Delete
             </Button>
-            <Button className="h-[35px] shadow-md">View</Button>
+            <Link href={`/users/${user.id}`}>
+              <Button className="h-[35px] shadow-md">View</Button>
+            </Link>
           </CardFooter>
         </Card>
       ))}
